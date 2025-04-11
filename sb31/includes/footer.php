fe70985a61a5ec6fe3b8ca18d58baf59a0091e86
TanTanH06 <natanielblanco01@gmail.com>
2025-04-11 16:09:55 +0800

    Add files via upload

--- sb31/includes/footer.php
+++ sb31/includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
+                    <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Your Website 2023</div>
                             <div>
@@ -10,5 +10,3 @@
                         </div>
                     </div>
                 </footer>
-            </div>
-        </div>
